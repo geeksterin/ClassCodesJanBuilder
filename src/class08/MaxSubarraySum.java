0cf55b41c57fd92f7ded2b6fd04a79ff188ba013
hparwani2 <himanshu.parwani8@gmail.com>
2021-01-24 20:05:16 +0530

    feat: class09

--- src/class08/MaxSubarraySum.java
+++ src/class08/MaxSubarraySum.java
@@ -11,20 +11,47 @@ public class MaxSubarraySum {
         for(int i=0;i<n;i++) {
             array[i] = scanner.nextInt();
         }
-        int sum = 0;
-        int maxsum = Integer.MIN_VALUE;
-        for(int i=0;i<n-1;i++) {
-            sum = array[i];
-            for(int j=i+1;j<n;j++) {
-                sum = sum + array[j]; //-5
-                maxsum = Math.max(sum, maxsum);
-            }
-        }
+        int max_ending_here = 0;
+        int max_till_now = Integer.MIN_VALUE;
+
         for(int i=0;i<n;i++) {
-            maxsum = Math.max(maxsum, array[i]);
+            max_ending_here = max_ending_here + array[i];
+            if(max_till_now < max_ending_here)
+                max_till_now = max_ending_here;
+            if(max_ending_here < 0)
+                max_ending_here = 0;
         }
-        System.out.println(maxsum);
+        System.out.println(max_till_now);
     }
 }
 
+//1 0 1 1 0 0 1 0 1 0
 
+//0 0 0 0 0
+
+
+//1 1 1 1 1 0 0 0 0 0
+
+
+//1. compare the element if it is 0 move it to the last else keep it as it is
+
+//count no of zeros and create new array
+//shifting to the right
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-2, -3, 4
+
+
+//we found all the subarrays and calculated the max sum
---
+++ src/class09/ShiftingZero.java
@@ -0,0 +1,41 @@
+package class09;
+
+import java.util.Scanner;
+
+public class ShiftingZero {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = scanner.nextInt(); //1 0 1 0 1 1 0 0 1
+        }
+        //iterate
+        int start = 0;
+        int end = n-1;
+        while(start < end) {
+            if(arr[start] == 0) {
+                swap(arr, start, end);
+                if(arr[start] == 1) {
+                    start++;
+                }
+                end--;
+            }
+            else  {
+                start++;
+            }
+        }
+        for(int element : arr) {
+            System.out.print(element + " ");
+        }
+    }
+    public static void swap(int arr[], int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+}
+
+//example
+//1 0 1 0 1 1 0 0 1
