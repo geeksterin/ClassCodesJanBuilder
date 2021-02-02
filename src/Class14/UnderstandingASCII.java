5d8b3e11aaccf93d26829825519a980236e0e552
bittoo aggarwal <bitz1119@gmail.com>
2021-02-02 22:00:06 +0530

    Clas14

---
+++ src/Class14/UnderstandingASCII.java
@@ -0,0 +1,28 @@
+package Class14;
+
+public class UnderstandingASCII {
+
+    public static void main(String[] args) {
+
+
+//        for (int i = 0; i < 127; i++) {
+//            System.out.println(i + " " + (char)i);
+//        }
+
+        char a = 'a';
+        char b = 'b';
+
+        System.out.println('a' - 'd');
+        System.out.println('z' > 'a');
+        // A = 65 a = 97
+
+        //97 - 100  = -3
+        //97 - 98
+        //97 + 98
+
+
+
+
+    }
+
+}
