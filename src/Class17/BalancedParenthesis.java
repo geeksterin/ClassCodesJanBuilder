fd1b75817a13645c511961b9cea7e2dcd61f595f
bittoo aggarwal <bitz1119@gmail.com>
2021-02-13 12:55:08 +0530

    Class17"

--- src/Class17/BalancedParenthesis.java
+++ src/Class17/BalancedParenthesis.java
@@ -1,2 +1,52 @@
-package Class17;public class BalancedParenthesis {
+package Class17;
+
+import java.lang.reflect.Parameter;
+import java.util.Stack;
+
+public class BalancedParenthesis {
+
+    public static void main(String[] args) {
+
+        String str = "{}[](())()[}";
+        Stack<Character> s = new Stack<>();
+        boolean isBalanced = true;
+        for (int i = 0;i<str.length();i++){
+            char element = str.charAt(i);
+            if(element == '(' || element == '{' || element == '['){
+                s.push(element);
+            }
+            else{
+                if (s.isEmpty() == true){
+                    //imbalance
+                    isBalanced = false;
+                    break;
+                }
+                else if(element == ')' && s.peek()=='('){
+                    s.pop();
+                }
+                else if (element == '}' && s.peek()=='{'){
+                    s.pop();
+                }
+                else if (element == ']' && s.peek() =='['){
+                    s.pop();
+                }
+                else{
+                    isBalanced = false;
+                    break;
+                    //imbalanced
+                }
+            }
+        }
+
+        if (isBalanced == true){
+            System.out.println("Parenthesis is Balanced");
+        }
+        else {
+            System.out.println("paranthesis is imbalanced");
+        }
+
+    }
+
+
+
 }
--- src/Class17/PriorityQueueImplementation.java
+++ src/Class17/PriorityQueueImplementation.java
@@ -1,2 +1,28 @@
-package Class17;public class PriorityQueueImplementation {
+package Class17;
+
+import java.util.PriorityQueue;
+
+public class PriorityQueueImplementation {
+
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+
+        pq.add(20);
+        pq.add(30);
+        pq.add(10);
+        pq.add(5);
+
+
+        //10 -- 20
+        System.out.println(pq.peek());
+
+        System.out.println(pq.remove());
+
+        System.out.println(pq.peek());
+
+
+    }
+
 }
